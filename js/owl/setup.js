227088b6e18bca31cce151762c413592b347f9b6
Gledson Nunes <96912896+nunes1886@users.noreply.github.com>
2022-12-06 01:14:13 +0000

    Initial commit

---
+++ js/owl/setup.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function botao(){
+    document.getElementById("Olá").innerHTML
+}
+
+function redirecionar(){
+    window.open("http://www.google.com.br/");
+}
